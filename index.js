08c9a3fe0a6de675caf8c4d6261181062f42aa6e
noribra <noribra@gmail.com>
2024-01-05 12:11:58 +0300

    creat medlwear

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 import Express from "express";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
-import bodyParser from "body-parser";
-import morgan from "morgan";
 
 const _direname = dirname(fileURLToPath(import.meta.url));
 
 const app = Express();
 const port = 3000;
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(morgan("tiny"));
+function login(req, res, next) {
+  console.log("method is " + req.method);
+  console.log("url is " + req.url);
+  next();
+}
+app.use(login);
 app.post("/login", (req, res) => {
   res.send({
     email: req.body.email,
